8c7f18437ef177766b2ba648d2f97f3455be2706
MagdyElhefny <154082438+MagdyElhefny@users.noreply.github.com>
2024-05-09 23:55:00 +0200

    Updata

--- main.py
+++ main.py
@@ -5,24 +5,7 @@ import math
 '''
 Functions
 '''
-# Function to add in the entry of text display
-def button_click(char):
-    global calc_operator
-    calc_operator += str(char)
-    text_input.set(calc_operator)
 
-# Function to clear the whole entry of text display
-def button_clear_all():
-    global calc_operator
-    calc_operator = ""
-    text_input.set("")
-
-# Function to delete one by one from the last in the entry of text display
-def button_delete():
-    global calc_operator
-    text = calc_operator[:-1]
-    calc_operator = text
-    text_input.set(text)
 
 
  # Function to calculate the factorial of a number
