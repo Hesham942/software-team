1735b130bfa8c18724c2d9f73325ef314135b8a2
hadeer135 <154346647+hadeer135@users.noreply.github.com>
2024-05-11 00:06:10 +0300

    Update main.py
    confirm the 2 factorial functions

--- main.py
+++ main.py
@@ -26,7 +26,19 @@ def button_delete():
 
 
 
+ # Function to calculate the factorial of a number
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
 
+def fact_func():
+    global calc_operator
+    result = str(factorial(int(calc_operator)))
+    calc_operator = result
+    text_input.set(result)
+    
 # Function to calculate trigonometric numbers of an angle
 def trig_sin():
     global calc_operator
